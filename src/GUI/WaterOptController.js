3b0d49c063e93d8e1b7c98d33984d329470021c6
Sumit <jindalsumit1991@gmail.com>
2017-01-14 19:49:47 +0530

    Bug fixes

--- src/GUI/WaterOptController.js
+++ src/GUI/WaterOptController.js
@@ -9,6 +9,7 @@ var util  = require('util'),
 var multer  = require('multer');
 
 var client_binary;
+var isSimulationStarted = 1;
 var filenames;
 var obj,Type,startTime,duration,w12,w3,w4,w5,w6;
   app.use(express.static('assets'));
@@ -113,99 +114,131 @@ http.listen(app.get('port'), function(){
 })
 
 io.on('connection', function(socket) {
-   console.log('new user connected');
+    console.log('new user connected');
 
-   socket.on('message', function(message) {
-      //Delete the intermediate files created by simulation process
-      //fs.readdir('.', (err, files)=>{
-      // for (var i = 0, len = files.length; i < len; i++) {
-      //  var match = files[i].match(/en.*/);
-      // if(match !== null)
-      // fs.unlink(match[0]);
-         //}
-      //});
-   //   fs.readdir('.', (err, files)=>{
-   //      for (var i = 0, len = files.length; i < len; i++) {
-   //	    var match = files[i].match(/en.*/);
-   //	    if(match !== null)
-   //	      fs.unlink(match[0]);
-   //      }
-   //   });
-
-      obj = JSON.parse(message);
-      Type = obj.Type;
-      duration = obj.Duration;
-
-
-      if(Type == 0) {
-	 startTime = obj.StartTime;
-	 var file1 = './uploads/' + filenames.files[0].originalname;
-	 var file2 = './uploads/' + filenames.files[1].originalname;
-	 var file3 = './uploads/' + filenames.files[2].originalname;
-	 client_binary = spawn('../../build/Linux/epanet_wateropt' ,['0', file1, file2, file3, startTime, duration, w12, w3, w4, w5, w6]);
-      }
-
-      else if(Type == 1) {
-	 w12 = obj.w12;
-	 w3 = obj.w3;
-	 w4 = obj.w4;
-	 w5 = obj.w5;
-	 w6 = obj.w6;
-	 var file1 = './uploads/' + filenames.files[0].originalname;
-	 var file2 = './uploads/' + filenames.files[1].originalname;
-	 var file3 = './uploads/' + filenames.files[2].originalname;
-	 var file4 = './uploads/' + filenames.files[3].originalname;
-	 var file5 = './uploads/' + filenames.files[4].originalname;
-	 client_binary = spawn('../../build/Linux/epanet_wateropt' ,['1', file1, file2, file3, file4, file5, duration, w12, w3, w4, w5, w6]);
-      }
-
-      else if(Type == 2) {
-	 var file1 = './uploads/' + filenames.files[0].originalname;
-	 var file2 = './uploads/' + filenames.files[1].originalname;
-	 var file3 = './uploads/' + filenames.files[2].originalname;
-	 var file4 = './uploads/' + filenames.files[3].originalname;
-	 var file5 = './uploads/' + filenames.files[4].originalname;
-	 client_binary = spawn('../../build/Linux/epanet_wateropt' ,['2', file1, file2, file3, file4, file5]);
-      }
-
-      var msg = {};
-      var chunk = '';
-
-      /* Registering event listeners on first client binary execution */    
-
-      /* Do some action whenever there is some data on standard
-	 output due to first binary execution */
-      if(client_binary) {
-	 client_binary.stdout.on('data', function(data){
-	    msg.Data = chunk + data;
-	    socket.emit('newdata', JSON.stringify(msg));
-	 });
-
-	 // Send a response to browser/client when first binary execution is completed
-
-	 client_binary.on('close', function (code) {
-	    msg = {};
-            if(code == '0'){
-	        msg.Result = 'All Iterations completed successfully, exit code : ' + code;
+    socket.on('message', function(message) {
+        //Delete the intermediate files created by simulation process
+        fs.readdir('.', (err, files)=>{
+            for (var i = 0, len = files.length; i < len; i++) {
+                var match = files[i].match(/en.*/);
+                if(match !== null)
+                    fs.unlink(match[0]);
             }
-            else{
-                msg.Result = 'Simulation ended abnormally, exit code : ' + code;
+        });
+        //   fs.readdir('.', (err, files)=>{
+        //      for (var i = 0, len = files.length; i < len; i++) {
+        //	    var match = files[i].match(/en.*/);
+        //	    if(match !== null)
+        //	      fs.unlink(match[0]);
+        //      }
+        //   });
+
+        obj = JSON.parse(message);
+        Type = obj.Type;
+        duration = obj.Duration;
+
+        console.log(obj);
+
+        if(Type == 0) {
+            startTime = obj.StartTime;
+            w12 = obj.w12;
+            w3 = obj.w3;
+            w4 = obj.w4;
+            w5 = obj.w5;
+            w6 = obj.w6;
+            var file1 = './uploads/' + filenames.files[0].originalname;
+            var file2 = './uploads/' + filenames.files[1].originalname;
+            var file3 = './uploads/' + filenames.files[2].originalname;
+            client_binary = spawn('../../build/Linux/epanet_wateropt' ,['0', file1, file2, file3, startTime, duration, w12, w3, w4, w5, w6]);
+            client_binary.on('error', (err) => {
+                isSimulationStarted = 0;
+                var msg={};
+                msg.Result = "Unable to spawn binary at path" + err.path;
+                console.log('whoops! There was an uncaught error', JSON.stringify(msg));
+                console.log('\nisSimulationStarted: ' + isSimulationStarted);
+                socket.emit('newdata', JSON.stringify(msg));
+            });
+        }
+
+        else if(Type == 1) {
+            w12 = obj.w12;
+            w3 = obj.w3;
+            w4 = obj.w4;
+            w5 = obj.w5;
+            w6 = obj.w6;
+            var file1 = './uploads/' + filenames.files[0].originalname;
+            var file2 = './uploads/' + filenames.files[1].originalname;
+            var file3 = './uploads/' + filenames.files[2].originalname;
+            var file4 = './uploads/' + filenames.files[3].originalname;
+            var file5 = './uploads/' + filenames.files[4].originalname;
+            client_binary = spawn('../../build/Linux/epanet_wateropt' ,['1', file1, file2, file3, file4, file5, duration, w12, w3, w4, w5, w6]);
+            client_binary.on('error', (err) => {
+                isSimulationStarted = 0;
+                var msg={};
+                msg.Result = "Unable to spawn binary at path" + err.path;
+                console.log('whoops! There was an uncaught error', JSON.stringify(msg));
+                socket.emit('newdata', JSON.stringify(msg));
+            });
+        }
+
+        else if(Type == 2) {
+            var file1 = './uploads/' + filenames.files[0].originalname;
+            var file2 = './uploads/' + filenames.files[1].originalname;
+            var file3 = './uploads/' + filenames.files[2].originalname;
+            var file4 = './uploads/' + filenames.files[3].originalname;
+            var file5 = './uploads/' + filenames.files[4].originalname;
+            client_binary = spawn('../../build/Linux/epanet_wateropt' ,['2', file1, file2, file3, file4, file5]);
+            client_binary.on('error', (err) => {
+                isSimulationStarted = 0;
+                var msg={};
+                msg.Result = "Unable to spawn binary at path" + err.path;
+                console.log('whoops! There was an uncaught error', JSON.stringify(msg));
+                console.log('\nisSimulationStarted: ' + isSimulationStarted);
+                socket.emit('newdata', JSON.stringify(msg));
+            });
+        }
+
+        var msg = {};
+        var chunk = '';
+
+        /* Registering event listeners on first client binary execution */    
+
+        /* Do some action whenever there is some data on standard
+           output due to first binary execution */
+        if(client_binary) {
+            console.log('\nFLAG: ' + isSimulationStarted + '\n');
+            if(isSimulationStarted) {
+                client_binary.stdout.on('data', function(data){
+                    msg.Data = chunk + data;
+                    socket.emit('newdata', JSON.stringify(msg));
+                });
+
+                // Send a response to browser/client when first binary execution is completed
+
+                client_binary.on('close', function (code) {
+                    msg = {};
+                    if(code == '0'){
+                        msg.Result = 'All Iterations completed successfully, exit code : ' + code;
+                    }
+                    else{
+                        msg.Result = 'Simulation ended abnormally, exit code : ' + code;
+                    }
+                    msg.ExitCode = code;
+                    socket.emit('newdata', JSON.stringify(msg));
+                    console.log('Process closed.');
+                    client_binary = null;
+                });
+
+
+                client_binary.stderr.on('data', function (data) {
+                    console.log('Failed to start child process 1.');
+                });
             }
-	    msg.ExitCode = code;
-	    socket.emit('newdata', JSON.stringify(msg));
-	    console.log('Process closed.');
-	    client_binary = null;
-	 });
-
-
-	 client_binary.stderr.on('data', function (data) {
-	    console.log('Failed to start child process 1.');
-	 });
-      }
-      else {
-	 msg = {};
-	 msg.log = 'Error: Simulation cannot be started!';
-	 socket.emit('newdata', JSON.stringify(msg));
-      }
-   });
+            else {
+                msg = {};
+                msg.Result = 'Error: Simulation cannot be started!';
+                socket.emit('newdata', JSON.stringify(msg));
+            }
+        }
+    });
 })
